639fe7a0659bb0e4514f08644ab62b6b7896b192
Srishti Mathur <srish.mathur12@gmail.com>
2023-10-19 13:37:01 +0530

    UI COntroller

---
+++ employeemanagement/src/main/java/com/empdetail/employeemanagement/UIController.java
@@ -0,0 +1,67 @@
+package com.empdetail.employeemanagement;
+
+import com.empdetail.employeemanagement.model.Employee;
+import com.empdetail.employeemanagement.service.EmployeeService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/home")
+public class UIController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/")
+    public String home(Model m) {
+        return findPaginated(0, m);
+    }
+
+    @GetMapping("/page/{pageno}")
+    public String findPaginated(@PathVariable int pageno, Model m) {
+
+        Page<Employee> emplist = employeeService.getEMpByPaginate(pageno, 10);
+        m.addAttribute("employee", emplist);
+        m.addAttribute("currentPage", pageno);
+        m.addAttribute("totalPages", emplist.getTotalPages());
+        m.addAttribute("totalItem", emplist.getTotalElements());
+        return "index";
+    }
+
+    @GetMapping("/addemp")
+    public String addEmpForm() {
+        return "add-emp";
+    }
+
+    @PostMapping("/register")
+    public String empRegister(@ModelAttribute Employee e, HttpSession session) {
+        employeeService.addNewEmployee(e);
+        session.setAttribute("msg", "Emplyoee Added Sucessfully..");
+        return "redirect:/home/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model m) {
+        Employee e = employeeService.findEmployeeById(id);
+        m.addAttribute("employee", e);
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String updateEmp(@ModelAttribute Employee e, HttpSession session) {
+        employeeService.addNewEmployee(e);
+        session.setAttribute("msg", "Emp Data Update Sucessfully..");
+        return "redirect:/home/";
+    }
+
+    /* Delete employee */
+    @GetMapping("/delete/{id}")
+    public String deleteEMp(@PathVariable Long id, HttpSession session) {
+        employeeService.deleteEmployee(id);
+        session.setAttribute("msg", "Emp Data Delete Sucessfully..");
+        return "redirect:/home/";
+    }
+}
--- employeemanagement/src/main/java/com/empdetail/employeemanagement/controller/EmployeeController.java
+++ employeemanagement/src/main/java/com/empdetail/employeemanagement/controller/EmployeeController.java
@@ -1,16 +1,13 @@
 package com.empdetail.employeemanagement.controller;
 
-import java.util.List;
-
-import jakarta.servlet.http.HttpSession;
+import com.empdetail.employeemanagement.model.Employee;
+import com.empdetail.employeemanagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.ui.Model;
-import com.empdetail.employeemanagement.model.Employee;
-import com.empdetail.employeemanagement.service.EmployeeService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
@@ -19,55 +16,6 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @GetMapping("/")
-    public String home(Model m) {
-        return findPaginated(0, m);
-    }
-
-    @GetMapping("/page/{pageno}")
-    public String findPaginated(@PathVariable int pageno, Model m) {
-
-        Page<Employee> emplist = employeeService.getEMpByPaginate(pageno, 10);
-        m.addAttribute("employee", emplist);
-        m.addAttribute("currentPage", pageno);
-        m.addAttribute("totalPages", emplist.getTotalPages());
-        m.addAttribute("totalItem", emplist.getTotalElements());
-        return "index";
-    }
-
-    @GetMapping("/addemp")
-    public String addEmpForm() {
-        return "add-emp";
-    }
-
-    @PostMapping("/register")
-    public String empRegister(@ModelAttribute Employee e, HttpSession session) {
-        employeeService.addNewEmployee(e);
-        session.setAttribute("msg", "Emplyoee Added Sucessfully..");
-        return "redirect:/home/";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Long id, Model m) {
-        Employee e = employeeService.findEmployeeById(id);
-        m.addAttribute("employee", e);
-        return "edit";
-    }
-
-    @PostMapping("/update")
-    public String updateEmp(@ModelAttribute Employee e, HttpSession session) {
-        employeeService.addNewEmployee(e);
-        session.setAttribute("msg", "Emp Data Update Sucessfully..");
-        return "redirect:/home/";
-    }
-
-    /* Delete employee */
-    @GetMapping("/delete/{id}")
-    public String deleteEMp(@PathVariable Long id, HttpSession session) {
-        employeeService.deleteEmployee(id);
-        session.setAttribute("msg", "Emp Data Delete Sucessfully..");
-        return "redirect:/home/";
-    }
 
     @GetMapping("/employee")
     public ResponseEntity<List<Employee>> getAllEmployee() {
